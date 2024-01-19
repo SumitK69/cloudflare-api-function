9951cbf5ea5b62a11784c0d9035e7170d29ce814
sumit khobragade <sumitbk1010@gmail.com>
2024-01-19 11:35:46 +0530

    simple router demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-
 import Posts from "./components/posts";
-import Post from "./components/post";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Posts />} />
-      <Route path="/posts/:id" element={<Post />} />
     </Routes>
   );
 }
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-const Posts = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const resp = await fetch("/api/posts");
-      const postsResp = await resp.json();
-      setPosts(postsResp);
-    };
-
-    getPosts();
-  }, []);
-
-  return (
-    <div>
-      <h1>Posts</h1>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>
-            <Link to={`/posts/${post.id}`}>{post.title}</Link>
-          </h2>
-        </div>
-      ))}
-    </div>
-  );
-};
+function Posts() {
+  return <div>Posts slskdfjld</div>;
+}
 
 export default Posts;
